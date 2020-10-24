e2132261b0e5d027856ee4d96161e80d40848449
Narek Manukyan <Gendrarium@yandex.ru>
2020-10-24 20:53:44 +0300

    Добавить javaScript

---
+++ index.js
@@ -0,0 +1,38 @@
+let editButton = document.querySelector('.profile__edit-button');
+
+function editButtonHandler() {
+  let editForm = document.querySelector('.edit-form');
+  editForm.classList.remove('edit-form_display-none');
+}
+
+editButton.addEventListener('click', editButtonHandler);
+
+let closeButton = document.querySelector('.edit-form__close-button');
+
+function closeButtonHandler() {
+  let editForm = document.querySelector('.edit-form');
+  editForm.classList.add('edit-form_display-none');
+}
+
+closeButton.addEventListener('click', closeButtonHandler);
+
+let formElement = document.querySelector('.edit-form__form');
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let nameInput = document.querySelector('.name-input');
+    let jobInput = document.querySelector('.job-input');
+
+    let nameProfile= document.querySelector('.profile__title');
+    let jobProfile= document.querySelector('.profile__subtitle');
+
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+    closeButtonHandler();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
