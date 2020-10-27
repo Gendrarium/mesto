72caa4f2f5119999b5f84ff57ed71793f714c3d3
Narek Manukyan <Gendrarium@yandex.ru>
2020-10-27 03:27:46 +0300

    Исправить все ошибки javascript

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-﻿let editButton = document.querySelector('.profile__edit-button');
+﻿let editForm = document.querySelector('.edit-form');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.edit-form__close-button');
+let nameInput = document.querySelector('.edit-form__input_content_name');
+let jobInput = document.querySelector('.edit-form__input_content_job');
+let nameProfile = document.querySelector('.profile__title');
+let jobProfile = document.querySelector('.profile__subtitle');
 
 function editButtonHandler() {
-  let editForm = document.querySelector('.edit-form');
-  editForm.classList.remove('edit-form_display-none');
+  editForm.classList.add('edit-form_display-flex');
+
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
-
-editButton.addEventListener('click', editButtonHandler);
-
-let closeButton = document.querySelector('.edit-form__close-button');
 
 function closeButtonHandler() {
-  let editForm = document.querySelector('.edit-form');
-  editForm.classList.add('edit-form_display-none');
+  editForm.classList.remove('edit-form_display-flex');
 }
-
-closeButton.addEventListener('click', closeButtonHandler);
 
 let formElement = document.querySelector('.edit-form__form');
 
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = document.querySelector('.name-input');
-    let jobInput = document.querySelector('.job-input');
-
-    let nameProfile= document.querySelector('.profile__title');
-    let jobProfile= document.querySelector('.profile__subtitle');
 
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     closeButtonHandler();
 }
+
+editButton.addEventListener('click', editButtonHandler);
+closeButton.addEventListener('click', closeButtonHandler);
 formElement.addEventListener('submit', formSubmitHandler);
