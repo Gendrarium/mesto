616df06ee2d00f61c590f81afe69f54f82b50af2
Narek Manukyan <Gendrarium@yandex.ru>
2020-11-08 00:09:39 +0300

    Добавить плавное появление попапов

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,10 @@ const closeButtonAdd = editFormAdd.querySelector('.edit-form__close-button');
 const nameInputAdd = editFormAdd.querySelector('.edit-form__input_content_name');
 const linkInputAdd = editFormAdd.querySelector('.edit-form__input_content_link');
 const formElementAdd = editFormAdd.querySelector('.edit-form__form');
+const editFormImg = document.querySelector('.edit-form_button_img');
+const popupImage = editFormImg.querySelector('.edit-form__image');
+const popupCaption = editFormImg.querySelector('.edit-form__caption');
+const closeButtonImg = editFormImg.querySelector('.edit-form__close-button');
 const nameProfile = document.querySelector('.profile__title');
 const jobProfile = document.querySelector('.profile__subtitle');
 const initialCards = [
@@ -43,9 +47,17 @@ const initialCards = [
 initialCards.forEach(function(item) {
   const cardCopy = document.querySelector('.template-cards').content.cloneNode(true);
 
-  cardCopy.querySelector('.card__image').src = item.link;
-  cardCopy.querySelector('.card__image').alt = item.name;
+  const cardImage = cardCopy.querySelector('.card__image');
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
   cardCopy.querySelector('.card__title').textContent = item.name;
+
+  cardImage.addEventListener('click',()=>{
+    popupImage.src = item.link;
+    popupCaption.textContent = item.name;
+
+    editFormImg.classList.add('edit-form_display-flex');
+  })
 
   const likeButton = cardCopy.querySelector('.card__like');
   likeButton.addEventListener('click', () => {
@@ -75,7 +87,9 @@ function closeButtonHandler() {
 function closeButtonAddHandler() {
   editFormAdd.classList.remove('edit-form_display-flex');
 }
-
+function closeButtonImgHandler() {
+  editFormImg.classList.remove('edit-form_display-flex');
+}
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
@@ -88,14 +102,24 @@ function formSubmitAddHandler (evt) {
   evt.preventDefault();
 
   const cardCopy = document.querySelector('.template-cards').content.cloneNode(true);
-  cardCopy.querySelector('.card__image').src = linkInputAdd.value;
-  cardCopy.querySelector('.card__image').alt = nameInputAdd.value;
+
+  const cardImage = cardCopy.querySelector('.card__image');
+  cardImage.src = linkInputAdd.value;
+  cardImage.alt = nameInputAdd.value;
   cardCopy.querySelector('.card__title').textContent = nameInputAdd.value;
+
+  cardImage.addEventListener('click',()=>{
+    popupImage.src = cardImage.src;
+    popupCaption.textContent = cardImage.alt;
+
+    editFormImg.classList.add('edit-form_display-flex');
+  })
 
   const likeButton = cardCopy.querySelector('.card__like');
   likeButton.addEventListener('click', () => {
     likeButton.classList.toggle('card__like_fill');
   });
+
   const delButton = cardCopy.querySelector('.card__del');
   delButton.addEventListener('click', () => {
     delButton.closest('.card').remove();
@@ -112,3 +136,4 @@ formElement.addEventListener('submit', formSubmitHandler);
 addButton.addEventListener('click', addButtonHandler);
 closeButtonAdd.addEventListener('click', closeButtonAddHandler);
 formElementAdd.addEventListener('submit', formSubmitAddHandler);
+closeButtonImg.addEventListener('click', closeButtonImgHandler);
